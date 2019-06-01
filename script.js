bb04c9523d2a46b8d9e843a95737ea70bca94e70
VitaliyTregubovKh <tregubovvakharkov@gmail.com>
2019-06-01 18:22:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class Todo{
+  constructor() {
+    this.todoList = [];
+  }
+  addItem(title, priority){
+    const newTask = {
+      title: title,
+      priority: priority,
+      id: parseInt(Math.random() * 10000000)
+    }
+    this.todoList.push(newTask);
+    console.log(this.todoList);
+  }
+  removeItem(id){
+    this.todoList = this.todoList.filter(item => item.id !== id);
+    const result = this.todoList.some(item => item.id == id);
+    console.log(result);
+  }
+  getItem(id){
+    const searchedTask = this.todoList.find(task => task.id === id) || null;
+    console.log(searchedTask);
+  }
+  next(){
+    if(this.todoList.length === 0) throw new Error('empty array!');
+    const sortedTasksByPriority = this.todoList.sort((prev, next) => next.priority - prev.priority); 
+    console.log(sortedTasksByPriority[0]);
+  }
+}
