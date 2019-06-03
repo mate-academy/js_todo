684f75ef79172277302d6907112a7628b61a3064
VladimirTi <tytmes@mail.ru>
2019-06-03 20:00:07 +0300

    Vladimir's Solution

---
+++ script.js
@@ -0,0 +1,50 @@
+class Todo {
+  constructor() {
+    this.id = 0;
+    this.list = [];
+  }
+
+  addItem(title, priority) {
+    const task = {id: this.id++, title, priority};
+    this.list.push(task);
+    return task.id;
+  }
+
+  removeItem(id) {
+    return (this.list.splice(this.list.findIndex(item => item.id === id), 1)).length > 0 ? true : false;
+  }
+
+  getItem(id) {
+    return this.list.find(item => item.id === id);
+  }
+
+  next() {
+    return this.list.reduce((acc, item) => {
+      if(item.priority < acc.priority) {
+        acc = item;
+      }
+      return acc;
+    })
+  }
+}
+
+function getRandomNum () {
+  return Math.floor(1 + Math.random() * 10);
+}
+
+const test1 = new Todo();
+
+test1.addItem('test2', 2);
+test1.addItem('test5', 5);
+test1.addItem('test1', 1);
+test1.addItem('test4', 4);
+test1.addItem('test6', 6);
+test1.addItem('test3', 3);
+test1.addItem('testHighest', 0);
+
+console.log(test1.removeItem(1));
+
+console.log(test1.getItem(3));
+console.log(test1.getItem(5));
+
+console.log(`The highest priority has ${test1.next().title}`);
