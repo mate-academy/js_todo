20da6c3eb377dd5a8c4afd2038a1e88d9d72eb4d
AlexHladun <gefest.gl@gmail.com>
2019-06-04 23:39:21 +0300

    Solution 2.0

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
+'use strict';
+
 class Todo {
-    constructor() {
-        this.toDoList = [];
-        this.currId = 0;
+  constructor() {
+    this.toDoList = [];
+    this.currId = 0;
+  }
+  addItem(title, priority) {
+    const item = {
+      title: title,
+      priority: priority,
+      id: ++this.currId
     }
-    addItem(title, priority) {
-        const item = {
-            title: title,
-            priority: priority,
-            id: ++this.currId
-        }
-        this.toDoList.push(item);
-        return item.id;
+    this.toDoList.push(item);
+    return item.id;
+  }
+  removeItem(id) {
+    const index = this.toDoList.findIndex(item => item.id === id);
+    if (index !== -1) {
+      this.toDoList.splice(index, 1);
+      return true;
     }
+    return false;
+  }
 
-    removeItem(id) {
-        const index = this.toDoList.findIndex(item => item.id === id);
-        if (index !== -1) {
-            this.toDoList.splice(index, 1);
-        } else {
-            return false;
-        }
-    }
+  getItem(id) {
+    return this.toDoList.find(item => item.id === id) || null;
+  }
 
-    getItem(id) {
-        return this.toDoList.find(item => item.id === id) || null;
+  next() {
+    const pattern = this.toDoList[0];
+    if (this.toDoList.length > 1) {
+      this.toDoList.forEach(item => {
+        if(pattern.priority < item.priority) pattern = item;
+      })
+    } else {
+      return this.toDoList[0];
     }
-
-    next() {
-        const pattern = this.toDoList[0];
-        if (this.toDoList.length > 1) {
-            this.toDoList.forEach(item => {
-                if(pattern.priority < item.priority) pattern = item;
-            })
-        } else {
-            return this.toDoList[0];
-        }
-        if (this.toDoList.length === 0) {
-            throw new Error('TODOList is Empty');
-        }
+    if (this.toDoList.length === 0) {
+      throw new Error('TODOList is Empty');
     }
+  }
 }
