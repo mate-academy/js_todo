df2c2665a5b9a062ba769f5eb3666c5bc497f7b4
Antonenko Bohdan <bohdan1997@gmail.com>
2019-06-04 15:44:44 +0300

    'new_changes'

--- js.js
+++ js.js
@@ -1,48 +1,46 @@
 'use strict';
 
 class Todo {
-    constructor() {
-        this.list = [];
-        this.id = 0;
-    }
+  constructor() {
+      this.list = [];
+      this.id = 0;
+  }
 
-    addItem(title, priority) {
-        let newObj = {
-            title : title,
-            priority : priority,
-            id : this.id++
-        }
-        this.list.push(newObj);
-        return newObj.id;
-    }
+  addItem(title, priority) {
+      let newObj = {
+          title : title,
+          priority : priority,
+          id : this.id++
+      }
+      this.list.push(newObj);
+      return newObj.id;
+  }
 
-    removeItem(id) {
-        let elIndex = this.list.findIndex(newObj => newObj.id ===id);
-        if (elIndex !== -1) {
-            this.list.splice(elIndex, 1);
-            return true;
-        } return false;
-    }
+  removeItem(id) {
+      let elIndex = this.list.findIndex(newObj => newObj.id ===id);
+      if (elIndex !== -1) {
+          this.list.splice(elIndex, 1);
+          return true;
+      }
 
-    getItem(id) {
-        let foundObj = this.list.find(newObj => newObj.id === id);
-        return foundObj || null;
-    }
+      return false;
+  }
 
-    next() {
-        try {
-            if (this.list.length === 0) {
-                throw new Error('Error');
-            }
-            this.list.sort((a, b) => {
-                if(a.priority < b.priority) return 1;
-                if(a.priority > b.priority) return -1;
-                if(a.priority === b.priority) return 0;
-            });
-            return this.list[0];
-        } catch(error) {
-            console.log('error');
-            return false;
-        }
-    }
+  getItem(id) {
+      let foundObj = this.list.find(newObj => newObj.id === id);
+      return foundObj || null;
+  }
+
+  next() {
+          if (this.list.length === 0) {
+              throw new Error('Error');
+          }
+          this.list.sort((a, b) => {
+              if(a.priority < b.priority) return 1;
+              if(a.priority > b.priority) return -1;
+              if(a.priority === b.priority) return 0;
+          });
+
+          return this.list[0];
+      }
 }
