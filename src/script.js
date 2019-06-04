86e28f9416f232553320d0d018016be193acc3ec
AnyaLisna <anyalesnaya2812@gmail.com>
2019-06-04 17:21:28 +0300

    fixed after TA comments

--- src/script.js
+++ src/script.js
@@ -1,33 +1,35 @@
 'use strict';
 
 class TODO {
-    constructor() {
-        this.todo = [];
-        this.id = 0;
-    }
+  constructor() {
+    this.todo = [];
+    this.id = 0;
+  }
 
-    addItem(title, priority) {
-       this.id++;
-       this.todo.push({id: this.id, title: `${title}`, priority: priority});
-       return this.id;
-    }
+  addItem(title, priority) {
+    this.id++;
+    this.todo.push({id: this.id, title: `${title}`, priority: priority});
+    return this.id;
+  }
 
-    removeItem(id) {
-       const index = this.todo.findIndex((item)=>item.id===id);
-       if(index < 0) {
-           return false;
-       } else {
-           this.todo.splice(index, 1);
-           return true;
-       }
-    }
+  removeItem(id) {
+    const index = this.todo.findIndex(item => item.id === id);
+    if(index === -1) {
+      return false;
+    } 
+    this.todo.splice(index, 1);
+    return true;    
+  }
 
-    getItem(id) {
-        return this.todo.find((item)=>item.id === id) || null;
-    }
+  getItem(id) {
+    return this.todo.find(item => item.id === id) || null;
+  }
 
-    next() {
-       this.todo.sort((a, b)=> b.priority - a.priority);
-       return this.todo[0];
+  next() {
+    if(this.todo.length) {
+      this.todo.sort((a, b) => b.priority - a.priority);
+      return this.todo[0];
     }
+    throw new Error('There are no items')
+  }
 }
