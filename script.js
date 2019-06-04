411703d0615af30ef46bf53e9265292a8a4ba37d
VitaliyTregubovKh <tregubovvakharkov@gmail.com>
2019-06-05 00:42:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 'use strict';
 
-class Todo{
+class Todo {
   constructor() {
     this.todoList = [];
   }
-  addItem(title, priority){
+
+  addItem(title, priority) {
     const newTask = {
       title: title,
       priority: priority,
       id: parseInt(Math.random() * 10000000)
     }
     this.todoList.push(newTask);
-    console.log(this.todoList);
+    return newTask.id;
   }
-  removeItem(id){
-    const result = this.todoList.some(item => item.id === id);
+
+  removeItem(id) {
     this.todoList = this.todoList.filter(item => item.id !== id);
-    console.log(result);
+    return this.todoList;
   }
-  getItem(id){
-    const searchedTask = this.todoList.find(task => task.id === id) || null;
-    console.log(searchedTask);
+
+  getItem(id) {
+    const searchedTask = this.todoList.find(task => task.id === id);
+    return searchedTask;
   }
-  next(){
-    if(this.todoList.length === 0) throw new Error('empty array!');
-    const sortedTasksByPriority = this.todoList.sort((prev, next) => next.priority - prev.priority); 
-    console.log(sortedTasksByPriority[0]);
+
+  next() {
+    if (this.todoList.length === 0) {
+      throw new Error('empty array!');
+    }
+    const sortedTasksByPriority = this.todoList.sort((prev, next) => next.priority - prev.priority);
+    return sortedTasksByPriority[0];
   }
 }
