dc5e5e5d7bc5cc61cc02c43021867cf86c4bd167
StasGostev <spaceomen@gmail.com>
2019-06-07 15:54:49 +0300

    'Solution_Todo'

---
+++ main.js
@@ -0,0 +1,41 @@
+"use strict";
+
+class Todo {
+  constructor() {
+    this.todoList = [];
+    this.Ids = 1;
+  }
+
+  addItem(title, priority) {
+    const newTask = {
+      title: title,
+      priority: priority
+    };
+
+    newTask.id = this.Ids;
+    this.todoList.push(newTask);
+    this.Ids++;
+    return newTask.id;
+  }
+
+  removeItem(id) {
+    const index = this.finder(id);
+    this.todoList.splice(index, 1);
+  }
+
+  getItem(id) {
+    return this.todoList[this.finder(id)] || null;
+  }
+
+  next() {
+    if (this.todoList.length === 0) {
+      throw new Error("Todo list is empty.");
+    }
+    const sorted = this.todoList.sort((a, b) => a.priority - b.priority);
+    return sorted[0];
+  }
+
+  finder(id) {
+    return this.todoList.findIndex(i => i.id === id);
+  }
+}
