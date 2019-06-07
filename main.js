1d4254bbd197c10fee19387fc226dc95af5b7310
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-06-07 11:14:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
+'use strict';
+
 class Todo {
-    constructor() {
-        this.todoArray = [];
-        this.counter = 0;
+  constructor() {
+    this.todoArray = [];
+    this.counter = 0;
+  }
+
+  addItem(title, priority) {
+    this.todoArray.push({
+      "title": title,
+      "priority": priority,
+      "id": this.counter
+    });
+
+    return this.counter++;
+  }
+
+  removeItem(id) {
+    let startLength = this.todoArray.length;
+    this.todoArray = this.todoArray.filter((value) => {
+      return value.id !== id;
+    });
+
+    return this.todoArray.length < startLength ? true : false;
+  }
+
+  getItem(id) {
+    let itemInList = this.todoArray.findIndex((value) => {
+      return value.id === id;
+    });
+
+    if (itemInList >= 0) {
+      return this.todoArray[itemInList];
     }
+    return null;
+  }
 
-    addItem(title, priority) {
-        this.todoArray.push({
-            "title": title,
-            "priority": priority,
-            "id": this.counter
-        });
+  next() {
+    if (this.todoArray.length === 0) throw "exception";
 
-        return this.counter++;
-    }
-
-    removeItem(id) {
-        let startLength = this.todoArray.length;
-        this.todoArray = this.todoArray.filter((value) => {
-            return value.id !== id;
-        });
-
-        return this.todoArray.length < startLength ? true : false;
-    }
-
-    getItem(id) {
-        let itemInList = this.todoArray.filter((value) => {
-            return value.id === id;
-        });
-
-        if (itemInList.length > 0) {
-            return itemInList[0];
-        }
-        return null;
-    }
-
-    next() {
-        if (this.todoArray.length === 0) throw "exception";
-
-        return this.todoArray.reduce(function (prev, current) {
-            return (prev.priority >= current.priority) ? prev : current
-        });
-    }
+    return this.todoArray.reduce(function (prev, current) {
+      return (prev.priority >= current.priority) ? prev : current
+    });
+  }
 }
 
