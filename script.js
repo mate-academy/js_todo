00cb3349b023e92898d0388ebb0ac50a9f019747
Andrii <farikpite@gmail.com>
2019-10-14 21:50:39 +0300

    Solution

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 class Todo {
     constructor() {
         this.arr = [];
+        this.id = 1;
     }
   
     addItem(title, priority) {
-         return this.arr.push({ title: title, priority: priority })
+         return this.arr.push({ title: title, priority: priority, id: this.id++ })
     }
     
     removeItem(id) { 
-        if (arr[id]) {
-            arr.splice(id, 1);
-            return true;
-        }  
-        return false;
+        for (let item = 0; item < arr.length; item++) {
+            if (item[i].id === id) {
+                arr.splice(i, 1);
+                return true;
+            }  
+            return false;
+        }
     }
   
     getItem(id) {
-        if (!arr[id]) {
+        for (let item of arr) {
+            if (item.id === id) {
+                return item;
+            }
             return null;
         }
-        return arr[id];
     }
   
     next() {
-        if(arr.length === 0) {
-            throw 'error!';
+        if (!arr.length){
+            throw new Error('error!');
         }
         let item = arr[0];
         for (let i = 0; i < arr.length; i++) {      
