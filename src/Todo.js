d3c1dbeaf3a1a8e3cbc32c2e2b19cd90486dd7bb
Liliia <chuiko1112@gmail.com>
2020-06-09 15:27:03 +0300

    solution

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,45 @@
 'use strict';
 
 class Todo {
-  // write code here
+  constructor() {
+    this.listOfItems = [];
+    this.idNumber = -1;
+  }
+  addItem(titleInput, priorityInput) {
+    this.idNumber++;
+
+    this.listOfItems[this.idNumber] = {
+      id: this.idNumber,
+      priority: priorityInput,
+      title: titleInput,
+    };
+
+    return this.idNumber;
+  }
+  removeItem(id) {
+    if (this.listOfItems.find(element => element.id === id) !== undefined) {
+      this.listOfItems.splice(id, 1);
+
+      return true;
+    }
+
+    return false;
+  }
+
+  getItem(id) {
+    return (this.listOfItems.hasOwnProperty(id))
+      ? this.listOfItems[id]
+      : null;
+  }
+  next() {
+    const priorityList = this.listOfItems.map(element => element.priority);
+    const output = this.listOfItems
+      .find(element => element.priority === Math.max.apply(null, priorityList));
+
+    return (output !== undefined)
+      ? output
+      : Error;
+  }
 }
 
 module.exports = {
