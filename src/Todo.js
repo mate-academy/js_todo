15f51b86f2fcacc0d53719c3999dd740f93883c3
DimaDP <dimadp95@gmail.com>
2020-06-09 18:40:57 +0300

    dimadp__solution

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,39 @@
 'use strict';
 
 class Todo {
-  // write code here
+  constructor() {
+    this.list = [];
+    this.idCount = 1;
+  }
+  addItem(title, priority) {
+    this.list.push({
+      'title': title, 'priority': priority, 'id': this.idCount,
+    });
+    this.idCount++;
+
+    return this.idCount - 1;
+  };
+  removeItem(id) {
+    if (this.list.some(item => item.id === id)) {
+      this.list = this.list.filter(item => item.id !== id);
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+  getItem(id) {
+    const findElem = this.list.find(item => item.id === id);
+
+    return (findElem !== undefined)
+      ? findElem : null;
+  }
+  next() {
+    const maxPriorityItem = this.list.find(item =>
+      item.priority === Math.max(...this.list.map(x => x.priority)));
+
+    return maxPriorityItem || 'error';
+  }
 }
 
 module.exports = {
