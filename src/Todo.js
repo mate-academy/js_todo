4255c43713288f59c746adbfde26e4014322c142
aleksey-10 <alexcare1337@gmail.com>
2020-06-09 17:50:46 +0300

    solution

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,47 @@
 'use strict';
 
 class Todo {
-  // write code here
+  constructor() {
+    this.list = [];
+  }
+
+  addItem(title, priority) {
+    let id = this.list.length + 1;
+
+    while (this.list.some(item => item.id === id)) {
+      id = Math.ceil(Math.random() * (this.list.length + 1));
+    }
+
+    this.list.push({
+      title,
+      priority,
+      id,
+    });
+
+    return id;
+  }
+
+  removeItem(id) {
+    const checkItem = item => item.id !== id;
+
+    if (this.list.some(checkItem)) {
+      return false;
+    }
+
+    this.list.filter(checkItem);
+
+    return true;
+  }
+
+  getItem(id) {
+    return this.list.find(item => item.id === id) || null;
+  }
+
+  next() {
+    return this.list.reduce((previous, current) =>
+      current.priority > previous.priority ? current : previous)
+      || Error;
+  }
 }
 
 module.exports = {
