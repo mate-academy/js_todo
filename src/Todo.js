daad0ac1f884ef667df639eb9bfddc73505ad81c
kvalenty <krushunin89@gmail.com>
2020-06-10 16:43:21 +0300

    solution

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,74 @@
 'use strict';
 
 class Todo {
-  // write code here
+  constructor() {
+    this.items = [];
+  }
+
+  addItem(title, priority) {
+    const item = new TodoItem(title, priority);
+
+    this.items.push(item);
+
+    return item.id;
+  };
+
+  removeItem(id) {
+    const deleted = this.items.filter((item) => item.getId() !== id);
+
+    if (this.items.length > deleted.length) {
+      this.items = deleted;
+
+      return true;
+    } else {
+      return false;
+    };
+  };
+
+  getItem(id) {
+    const item = this.items.find((el) => (el.getId() === id));
+
+    if (!item) {
+      return null;
+    }
+
+    return item;
+  };
+
+  next() {
+    if (this.length === 0) {
+      throw Error;
+    }
+
+    let highestPriority = this.items[0];
+
+    this.items.forEach((item) => {
+      if (item.getPriority() > highestPriority.getPriority()) {
+        highestPriority = item;
+      }
+    });
+
+    return highestPriority;
+  };
 }
+
+class TodoItem {
+  constructor(title, priority) {
+    this.id = Math.floor(Math.random() * 1e5);
+    this.title = title;
+    this.priority = priority;
+  }
+
+  getId() {
+    return this.id;
+  };
+  getTitle() {
+    return this.title;
+  };
+  getPriority() {
+    return this.priority;
+  };
+};
 
 module.exports = {
   Todo,
