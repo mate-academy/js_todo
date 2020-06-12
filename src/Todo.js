274ac0847082264eeac5d6eca20c8dcc231ca2d0
DTR <t.terry.rice@gmail.com>
2020-06-12 21:33:21 +0300

    final 1.1

--- src/Todo.js
+++ src/Todo.js
@@ -2,22 +2,25 @@
 
 class Todo {
   constructor() {
-    this.name = {};
-    this.priority = {};
+    this.elements = [];
     this.lastID = 0;
   }
 
-  addItem(name, priority) {
-    this.name[this.lastID] = name;
-    this.priority[this.lastID] = priority;
+  addItem(title, priority) {
+    const id = this.lastID;
+
+    this.elements[this.lastID] = {
+      id,
+      title,
+      priority,
+    };
 
     return this.lastID++;
   }
 
   removeItem(id) {
-    if (this.name[id]) {
-      delete this.name[id];
-      delete this.priority[id];
+    if (this.elements[id]) {
+      this.elements.splice(id, 1);
 
       return true;
     } else {
@@ -25,33 +28,20 @@ class Todo {
     }
   }
 
-  getItem(requestID) {
-    const id = requestID;
-    const title = this.name[id];
-    const priority = this.priority[id];
-
-    if (this.name[id]) {
-      return ({
-        id, title, priority,
-      });
+  getItem(id) {
+    if (this.elements[id]) {
+      return (
+        this.elements[id]
+      );
     } else {
       return null;
     };
   }
 
   next() {
-    if (!this.name[0]) {
-      return 'Напривеликий жаль, примушений сповістити панство про помилку';
-    }
-
-    const allPriority = Object.values(this.priority);
-    const id = allPriority.indexOf(Math.max.apply(null, allPriority));
-    const priority = Object.entries(this.priority)[id][1];
-    const title = Object.entries(this.name)[id][1];
-
-    return ({
-      id, title, priority,
-    });
+    return this.elements.sort((a, b) => {
+      return b.priority - a.priority;
+    })[0];
   }
 }
 
