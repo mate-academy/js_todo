b9cf29bd610aa34f79441d27bb928438dfca9878
tyooma <flying.alien.godzilla@gmail.com>
2020-06-13 17:39:37 +0300

    TODO solution

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,52 @@
+/* eslint-disable no-shadow */
 'use strict';
 
+let id = 0;
+
+function idGenerator() {
+  id++;
+
+  return id;
+}
+
 class Todo {
-  // write code here
+  constructor() {
+    this.list = new Map();
+  }
+
+  addItem(title, priority) {
+    this.list.set(idGenerator(), {
+      id,
+      title,
+      priority,
+    });
+
+    return id;
+  }
+
+  removeItem(id) {
+    return this.list.delete(id);
+  }
+
+  getItem(id) {
+    return id && this.list.get(id) !== undefined ? this.list.get(id) : null;
+  }
+
+  next() {
+    const priorityList = [];
+
+    for (const p of this.list.entries()) {
+      priorityList.push(p[1].priority);
+    }
+
+    const max = Math.max.apply(null, priorityList);
+
+    for (const p of this.list.entries()) {
+      if (p[1].priority === max) {
+        return p[1];
+      }
+    }
+  }
 }
 
 module.exports = {
