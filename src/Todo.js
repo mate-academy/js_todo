5993e2b8476020d4fadfbcc20278973f21b423b7
Andrew Nickyforov <nik_andrey@ukr.net>
2020-06-15 14:05:57 +0300

    solution

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,42 @@
+/* eslint-disable no-console */
 'use strict';
 
 class Todo {
   // write code here
+  constructor() {
+    this.todoList = [];
+    this.id = 0;
+  }
+
+  addItem(title, priority) {
+    this.todoList.push({
+      title: title,
+      priority: priority,
+      id: ++this.id,
+    });
+
+    return this.id;
+  }
+
+  removeItem(id) {
+    return this.todoList.length !== this.todoList.filter(
+      item => item.id !== id
+    ).length;
+  }
+
+  getItem(id) {
+    return this.todoList.find(item => item.id === id) || null;
+  }
+
+  next() {
+    this.todoList.sort((a, b) => b.priority - a.priority);
+
+    if (!this.todoList.length) {
+      throw new Error('List is empty');
+    }
+
+    return this.todoList[0];
+  }
 }
 
 module.exports = {
