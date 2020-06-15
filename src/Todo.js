8d5e44b9fa7ec9a8f735ccead03e06b90a5fdd00
Stanislav <stasusss07@gmail.com>
2020-06-15 12:36:43 +0300

    solution

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,44 @@
 'use strict';
 
 class Todo {
-  // write code here
+  constructor() {
+    this.list = [];
+    this.id = 1;
+  }
+
+  addItem(title, priority) {
+    this.list.push({
+      title: title,
+      priority: priority,
+      id: this.id,
+    });
+
+    return this.id++;
+  }
+
+  removeItem(id) {
+    const index = this.list.findIndex(element => element.id === id);
+
+    if (index !== -1) {
+      this.list.splice(index, 1);
+
+      return true;
+    }
+
+    return false;
+  }
+
+  getItem(id) {
+    return this.list.find(element => element.id === id) || null;
+  }
+
+  next() {
+    if (this.list.length === 0) {
+      throw new Error(`It's empty`);
+    }
+
+    return this.list.sort((a, b) => b.priority - a.priority)[0];
+  }
 }
 
 module.exports = {
