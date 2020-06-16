50e41f46c442d90e8ac394d60a82cce63d8e7fef
Nazar <nazardovhanchuk@gmail.com>
2020-06-16 09:49:13 +0300

    solution

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,48 @@
 'use strict';
 
 class Todo {
-  // write code here
+  constructor() {
+    this.todo = [];
+    this.id = 0;
+  }
+
+  addItem(title, priority) {
+    this.todo.push({
+      title: title,
+      priority: priority,
+      id: this.id,
+    });
+
+    return this.id++;
+  }
+
+  removeItem(id) {
+    const deleted = this.todo.filter(value => value.id !== id);
+
+    if (this.todo.length > deleted.length) {
+      this.todo = deleted;
+
+      return true;
+    }
+
+    return false;
+  }
+
+  getItem(id) {
+    const item = this.todo.find((elements) => (elements.id === id));
+
+    return item || null;
+  };
+
+  next() {
+    if (this.length === 0) {
+      throw Error;
+    }
+
+    this.todo.sort((a, b) => b.priority - a.priority);
+
+    return this.todo[0];
+  }
 }
 
 module.exports = {
